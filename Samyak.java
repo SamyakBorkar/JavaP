868a75308072e7c3aa94beebf4e60960ba07a210
SamyakBorkar <2022bit037@sggs.ac.in>
2024-01-24 12:31:30 +0530

    24 jan

---
+++ Samyak.java
@@ -0,0 +1,17 @@
+public class Samyak{
+	public static void main(String[] args){
+		int arr[]={1,2,3,4,5,6,7,8,9,10};
+		 Myfunction();
+		 Printarray(arr);
+		
+		}
+		public static void Myfunction(){
+		 System.out.println("Hello Samyak");
+		}
+		
+		public static void Printarray(int[] args){
+			 for(int i=0;i<10;i++){
+			 System.out.println(args[i]);}
+		
+	        	}
+		}
