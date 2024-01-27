84d94c821db140bde88b74e53adcd4b992503a7b
SamyakBorkar <2022bit037@sggs.ac.in>
2024-01-27 15:44:38 +0530

    24jan Practical

---
+++ SY_2022bit037.java
@@ -0,0 +1,31 @@
+public class SY_2022bit037{
+	public static void main(String[] args){
+		 int arr[]={1,2,3,4,5,6,7,8,9,10};
+		 
+		 int x= firstele(arr);
+		System.out.println(x);
+		int y=lastele(arr);
+		System.out.println(y);
+		 int z=lassecele(arr);
+		 System.out.println(z);
+		 
+		}
+		
+		public static int firstele(int[] args){
+			return args[0];
+			}
+			
+		public static int lastele(int[] args){
+			return args[args.length-1];
+			}
+			
+		public static int lassecele(int[] args){
+		       return args[args.length-2];
+		       }
+		       
+		public static int sumele(int[] args){
+		      
+			
+		
+	}
+
--- Samyak.java
+++ Samyak.java
@@ -2,16 +2,23 @@ public class Samyak{
 	public static void main(String[] args){
 		int arr[]={1,2,3,4,5,6,7,8,9,10};
 		 Myfunction();
-		 Printarray(arr);
+		  int s=Printarray(arr);
+		  System.out.println(s);
 		
 		}
 		public static void Myfunction(){
 		 System.out.println("Hello Samyak");
 		}
 		
-		public static void Printarray(int[] args){
+		public static int Printarray(int[] args){
 			 for(int i=0;i<10;i++){
-			 System.out.println(args[i]);}
-		
+			 System.out.println(args[i]);
+			 }
+		     return 3;
 	        	}
 		}
+		
+		//return 1st ele of array
+		//returnn last ele of array
+		//return 2nd last 
+		//return sum of all ele of array
