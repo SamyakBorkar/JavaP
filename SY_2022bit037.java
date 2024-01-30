80696c2d70c0d725ad58d0ee9aa76140d58c8207
SamyakBorkar <2022bit037@sggs.ac.in>
2024-01-30 23:58:26 +0530

    Completed the sum of array method

--- SY_2022bit037.java
+++ SY_2022bit037.java
@@ -8,22 +8,31 @@ public class SY_2022bit037{
 		System.out.println(y);
 		 int z=lassecele(arr);
 		 System.out.println(z);
+		 int w=sumele(arr);
+		 System.out.println(w);
 		 
 		}
-		
+	      //Printing the first element of Array	
 		public static int firstele(int[] args){
 			return args[0];
 			}
-			
+		//Printing the last element of Array	
 		public static int lastele(int[] args){
 			return args[args.length-1];
 			}
-			
+		//Printing the Last Second Element of Array	
 		public static int lassecele(int[] args){
 		       return args[args.length-2];
 		       }
-		       
+		//Printing the Sum of Elements of Array       
 		public static int sumele(int[] args){
+			int sum=0;
+			for(int i=0;i<args.length;i++){
+			  	sum=sum+args[i];
+			  }
+			  return sum;
+		}
+		       
 		      
 			
 		
